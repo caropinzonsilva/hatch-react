3859eb8ff42d5972041de42a35ea56ea7ae26da1
Nick Salloum <nick@callmenick.com>
2017-01-04 10:14:18 -0400

    creates and structures header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 
 const Header = () => (
   <div className="Header">
-    <h1>hatch-react</h1>
+    <h1 className="Header-title">hatch-react</h1>
+    <p className="Header-subTitle">A React starter kit.</p>
   </div>
 );
 
