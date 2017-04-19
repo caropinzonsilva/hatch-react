be9d95748ce1c4be91f6b348949f0d18a1c6d9a5
Nick Salloum <nick@callmenick.com>
2017-04-19 11:31:28 -0700

    adds webpack env configs

---
+++ webpack/development.js
@@ -0,0 +1,21 @@
+import webpack from 'webpack';
+
+export default {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    overlay: true,
+    port: '3000'
+  },
+  devtool: 'cheap-eval-source-map',
+};
