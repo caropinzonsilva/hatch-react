b416652c00a12bf409036de9cfa9c8d7c1d2b3df
Felipe Torres <felipe@axiomzen.com>
2017-04-26 11:05:26 -0300

    Adding support for Redux DevTools Extension

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,18 +1,30 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers/index.js';
 
-function configureStore() {
-  const middlewares = [thunk];
+let composeEnhancers = compose;
+if (process.env.NODE_ENV !== 'production' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    name: 'MyApp', actionsBlacklist: ['REDUX_STORAGE_SAVE']
+  });
+}
 
+function configureStore(initialState) {
+  const middlewares = [thunk];
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger());
   }
 
+  const enhancer = composeEnhancers(
+    applyMiddleware(...middlewares),
+  );
+
   return createStore(
     reducer,
-    applyMiddleware(...middlewares)
+    initialState,
+    enhancer
   );
 }
 
