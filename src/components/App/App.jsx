440d12e2a278346db3c09f942f5f661e18abc020
Nick Salloum <nick@callmenick.com>
2017-05-30 11:00:24 -0700

    adds variable based styling to components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from 'components/Header/Header.jsx';
 import Main from 'components/Main/Main.jsx';
+import Footer from 'components/Footer/Footer.jsx';
 
 function App({ children }) {
   return (
     <div className="App">
       <Header />
       <Main>{children}</Main>
+      <Footer />
     </div>
   );
 }
