5d43a05adb70eb3b8b4ed14b3d5f0127fb27a4ec
Jake Runzer <jakerunzer@gmail.com>
2017-05-31 23:32:38 -0700

    use redux to manage story counter state

--- stories/Counter.jsx
+++ stories/Counter.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { Provider } from 'react-redux';
 
-// Components
-import Counter from 'components/Counter/Counter.jsx';
+import configureStore from 'configureStore.js';
+import CounterContainer from 'containers/CounterContainer/CounterContainer.jsx';
 
+const store = configureStore();
 const counterProps = {
   count: 0,
   increment: action('increment'),
   decrement: action('decrement')
 };
 
+const storeDecorator = getStory => (
+  <Provider store={store}>
+    { getStory() }
+  </Provider>
+);
+
 storiesOf('Counter', module)
-  .add('text', () => (
-    <Counter
+  .addDecorator(storeDecorator)
+  .add('Zero', () => (
+    <CounterContainer
       {...counterProps}
     />
   ));
