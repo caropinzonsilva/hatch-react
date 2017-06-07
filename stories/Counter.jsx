b157ca86e807a9fe0b65c0151c04df056dff473f
Jake Runzer <jakerunzer@gmail.com>
2017-06-07 11:25:49 -0700

    add another sub-story to counter

--- stories/Counter.jsx
+++ stories/Counter.jsx
@@ -5,15 +5,19 @@ import { action } from '@storybook/addon-actions';
 import { INCREMENT, DECREMENT } from 'actions/count.js';
 import Counter from 'components/Counter/Counter.jsx';
 
-const counterProps = {
-  count: 0,
+const counterProps = count => ({
+  count,
   increment: action(INCREMENT),
   decrement: action(DECREMENT)
-};
+});
 
 storiesOf('Counter', module)
   .add('with count initialized to 0', () =>
     <Counter
-      {...counterProps}
+      {...counterProps(0)}
+    />
+  ).add('with count initialized to 123', () =>
+    <Counter
+      {...counterProps(123)}
     />
   );
