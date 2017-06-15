160221d02fbe8da5ee5b7bf9fd7a46f7167716f3
Jake Runzer <jakerunzer@gmail.com>
2017-06-15 11:37:49 -0700

    remove redux actions from storybook

--- stories/Counter.jsx
+++ stories/Counter.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import { INCREMENT, DECREMENT } from 'actions/count.js';
 import Counter from 'components/Counter/Counter.jsx';
 
 const counterProps = count => ({
   count,
-  increment: action(INCREMENT),
-  decrement: action(DECREMENT)
+  increment: action('Increment'),
+  decrement: action('Decrement')
 });
 
 storiesOf('Counter', module)
