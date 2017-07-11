31e3312a3df9c0a00539c448f45672510a05ccf0
Nick Salloum <nick@callmenick.com>
2017-07-10 17:34:11 -0700

    renders navigation and routes

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import App from 'components/App/App.jsx';
-import CounterContainer from 'containers/CounterContainer/CounterContainer.jsx';
+import Container from 'components/Container/Container.jsx';
 
 function HomeView() {
   return (
-    <App>
-      <CounterContainer />
-    </App>
+    <Container>
+      <h1>Welcome to hatch-react</h1>
+      <p>hatch-react is a React starter kit. It features:</p>
+      <ul>
+        <li>React</li>
+        <li>Redux</li>
+        <li>React Router</li>
+        <li>Webpack</li>
+        <li>Sass</li>
+      </ul>
+    </Container>
   );
 }
 
