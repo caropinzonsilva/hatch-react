93e718bc3a0912fe48f88052eeb7d7ac48ea22aa
Arthur Camara <arthurcamara@gmail.com>
2017-10-17 19:13:57 -0700

    simplify AppView

--- src/views/AppView.jsx
+++ src/views/AppView.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import App from 'components/App/App.jsx';
+import { Route } from 'react-router-dom';
+import HomeView from 'views/HomeView.jsx';
+import CounterView from 'views/CounterView.jsx';
+import Header from 'components/Header/Header.jsx';
+import Main from 'components/Main/Main.jsx';
+import Footer from 'components/Footer/Footer.jsx';
 
 function AppView() {
   return (
-    <App />
+    <div>
+      <Header />
+      <Main>
+        <Route exact path="/" component={HomeView} />
+        <Route exact path="/counter" component={CounterView} />
+      </Main>
+      <Footer />
+    </div>
   );
 }
 
