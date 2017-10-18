c8dd1da6d477098f2cf711d6dfb9520aa53bd372
Arthur Camara <arthurcamara@gmail.com>
2017-10-17 19:10:25 -0700

    remove subfolders from views

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import configureStore from 'configureStore.js';
-import AppView from 'views/AppView/AppView.jsx';
+import AppView from 'views/AppView.jsx';
 import 'styles.scss';
 
 const store = configureStore();
