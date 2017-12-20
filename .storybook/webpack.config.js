ea7cd62cfbeeaafef32b29f6dbc8127bc2ffcb62
Maximiliano Castro <max@axiomzen.co>
2017-12-20 14:45:53 -0800

    more prettify

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,27 +2,32 @@ const path = require('path');
 
 module.exports = {
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader'
-      }, {
-        loader: 'postcss-loader'
-      }, {
-        loader: 'sass-loader',
-        options: {
-          outputStyle: 'expanded',
-          includePaths: [
-            path.resolve(__dirname, '../src/styles')
-          ]
-        }
-      }],
-      include: path.resolve()
-    }]
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              outputStyle: 'expanded',
+              includePaths: [path.resolve(__dirname, '../src/styles')],
+            },
+          },
+        ],
+        include: path.resolve(),
+      },
+    ],
   },
   resolve: {
-    modules: [path.resolve(__dirname, '../src'), 'node_modules']
-  }
+    modules: [path.resolve(__dirname, '../src'), 'node_modules'],
+  },
 };
