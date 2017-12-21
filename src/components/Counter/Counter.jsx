a769450114d786640705c330f57eea20811013cc
Maximiliano Castro <max@axiomzen.co>
2017-12-20 16:24:11 -0800

    revert change

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -6,7 +6,7 @@ const Counter = ({ count, increment, decrement }) => (
   <div className="Counter">
     <h2 className="Counter-title">Counter Component</h2>
     <div className="Counter-content">
-      <button className="Counterdsdsa-button" onClick={decrement}>
+      <button className="Counter-button" onClick={decrement}>
         -
       </button>
       <span className="Counter-count">{count}</span>
