9ccddd6d138c2921e8434debbcc0b6c937e702c7
Nick Salloum <nick@callmenick.com>
2018-03-26 12:26:57 -0700

    uses absolute path

--- app/middleware.js
+++ app/middleware.js
@@ -6,17 +6,28 @@ const favicon = require('serve-favicon');
 const helmet = require('helmet');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const path = require('path');
+
+const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = function(app) {
+  // force ssl in production
+  // if (NODE_ENV === 'production') {
+  //   app.use((req, res, next) => {
+  //     if (req.headers['x-forwarded-proto'] !== 'https') {
+  //       return res.redirect(301, `https://${req.get('Host')}${req.url}`);
+  //     }
+  //     return next();
+  //   });
+  // }
+
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(compress());
   app.use(cookieParser());
   app.use(express.static('dist'));
-  app.use(favicon('dist/favicon/favicon.ico'));
+  app.use(favicon(path.join(__dirname, '../dist/favicon/favicon.ico')));
   app.use(helmet.frameguard('deny'));
   app.use(methodOverride());
-  app.use(
-    process.env.NODE_ENV === 'production' ? morgan('combined') : morgan('dev'),
-  );
+  app.use(NODE_ENV === 'production' ? morgan('combined') : morgan('dev'));
 };
